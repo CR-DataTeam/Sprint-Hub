4e053ea57c93b12caabe28278fda78285701f9d4
Joshua McDonald <joshua.mcdonald@usradiology.com>
2023-01-10 14:54:09 -0500

    Update strConstants.py

--- strConstants.py
+++ strConstants.py
@@ -36,10 +36,6 @@ adjustPaddingAndFont = """
                     padding-left: 1rem;
                     padding-right: 1rem;
                 }
-               .css-wldksm {
-                    padding-top: 4rem;
-                    font-size: 0.75rem;
-                }
                .css-b7s55g {
                     padding-top: 0rem;
                     padding-right: 0rem;
@@ -61,17 +57,6 @@ adjustPaddingAndFont = """
                .css-1avcm0n {
                     height: 0rem;   
                 }
-               .css-uc76bn {
-                    padding-top: 4rem;
-                    font-size: 0.75rem;
-                }
-               .css-13eon2a {
-                    font-size: 0.75rem;
-                }
-               .css-79elbk {
-                    font-size: 0.75rem;
-                }
-        </style>
         """
         
 jsCodeStr = """
